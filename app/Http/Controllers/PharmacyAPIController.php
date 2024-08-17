155c0094977bcc272e94f654c2d5124a1670b385
nabil-rady <midorady9999@gmail.com>
2024-08-17 13:41:06 +0300

    Add api resource controllers for pharmacy.

---
+++ app/Http/Controllers/PharmacyAPIController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pharmacy;
+use Illuminate\Http\Request;
+
+class PharmacyAPIController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Pharmacy::paginate(10);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => ['required', 'min:3', 'max:255'],
+            'address' => ['required', 'min:3', 'max:255'],
+        ]);
+
+        $pharmacy = Pharmacy::create($data);
+
+        return $pharmacy;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pharmacy  $pharmacy
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pharmacy $pharmacy)
+    {
+        $products = $pharmacy->products()->paginate(10);
+
+        return [
+            'pharmacy' => $pharmacy,
+            'products' => $products,
+        ];
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pharmacy  $pharmacy
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pharmacy $pharmacy)
+    {
+        $data = $request->validate([
+            'name' => ['required', 'min:3', 'max:255'],
+            'address' => ['required', 'min:3', 'max:255'],
+        ]);
+
+        $pharmacy->update($data);
+
+        return $pharmacy;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pharmacy  $pharmacy
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pharmacy $pharmacy)
+    {
+        $pharmacy->delete();
+
+        return response()->json([], 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductsAPIController;
+use App\Http\Controllers\PharmacyAPIController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::apiResource('products', ProductsAPIController::class);
+Route::apiResource('pharmacies', PharmacyAPIController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
