a25b9927a567fa94adccf37713c7d7c44d45e852
nabil-rady <midorady9999@gmail.com>
2024-08-18 20:22:29 +0300

    Don't display table if no resources are there.

--- resources/views/pharmacies/index.blade.php
+++ resources/views/pharmacies/index.blade.php
@@ -5,32 +5,36 @@
         <h1>Pharmacies</h1>
         <a href="{{ route('pharmacies.create') }}" class="ms-auto btn btn-primary">Create New Pharmacy</a>
     </div>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Address</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($pharmacies as $pharmacy)
+    @if($pharmacies->isEmpty())
+        <p>No pharmacies found</p>
+    @else
+        <table class="table">
+            <thead>
                 <tr>
-                    <td>{{ $pharmacy->id }}</td>
-                    <td>{{ $pharmacy->name }}</td>
-                    <td>{{ $pharmacy->address }}</td>
-                    <td>
-                        <a href="{{ route('pharmacies.show', $pharmacy) }}" class="btn btn-info">View</a>
-                        <a href="{{ route('pharmacies.edit', $pharmacy) }}" class="btn btn-warning">Edit</a>
-                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteConfirmationModal{{$pharmacy->id}}">
-                            Delete
-                        </button>
-                    </td>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Address</th>
+                    <th>Actions</th>
                 </tr>
-                <x-delete-confirmation-modal resourceName="pharmacy" :resource="$pharmacy" />
-                @endforeach
-            </tbody>
-        </table>
+            </thead>
+            <tbody>
+                @foreach ($pharmacies as $pharmacy)
+                    <tr>
+                        <td>{{ $pharmacy->id }}</td>
+                        <td>{{ $pharmacy->name }}</td>
+                        <td>{{ $pharmacy->address }}</td>
+                        <td>
+                            <a href="{{ route('pharmacies.show', $pharmacy) }}" class="btn btn-info">View</a>
+                            <a href="{{ route('pharmacies.edit', $pharmacy) }}" class="btn btn-warning">Edit</a>
+                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteConfirmationModal{{$pharmacy->id}}">
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
+                    <x-delete-confirmation-modal resourceName="pharmacy" :resource="$pharmacy" />
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
     {{ $pharmacies->links() }}
 @endsection
